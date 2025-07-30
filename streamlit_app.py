6def513c7c42a336d373e05e352650a8ce25f9c9
divyapriyab <divya.priyab@i2econsulting.com>
2025-07-30 16:40:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,11 +39,11 @@ my_insert_stmt = f"""
         INSERT INTO smoothies.public.orders(ingredients, name_on_order)
         VALUES ('{ingredients_string}', '{name_on_order}')
     """
-    st.write(my_insert_stmt)
+st.write(my_insert_stmt)
 
-    time_to_insert = st.button('Submit Order')
+time_to_insert = st.button('Submit Order')
 
-    if time_to_insert:
+if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
