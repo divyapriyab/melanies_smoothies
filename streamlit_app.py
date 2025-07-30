4f3ea1783fd52a84018002d901cf0fbda94292bf
divyapriyab <divya.priyab@i2econsulting.com>
2025-07-30 16:34:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ if ingredients_list:
     ingredients_string = ' '.join(ingredients_list)
     st.subheader(ingredients_string + 'Nutrition Information')
     for fruit_chose in ingredients_list:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-    sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
   
 
     my_insert_stmt = f"""
